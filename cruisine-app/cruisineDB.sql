cada869577b00d0668c999ed8d62cd64eaf17842
Zac Eskridge <zace118@gmail.com>
2020-04-24 11:46:03 -0700

    Editted the SQLZE models a bit and added in TopNav

--- cruisine-app/cruisineDB.sql
+++ cruisine-app/cruisineDB.sql
@@ -4,70 +4,73 @@ CREATE DATABASE cruisineDB;
 
 USE cruisineDB;
 
-CREATE TABLE users (
-	user_id INT NOT NULL AUTO_INCREMENT,
-    email VARCHAR(45) NOT NULL,
-    user_name VARCHAR(30) NOT NULL, 
-    user_pass VARCHAR(30) NOT NULL,
-    truck_id INT, 
-    PRIMARY KEY (user_id)
-);
 
-CREATE TABLE trucks(
-	truck_id INT NOT NULL AUTO_INCREMENT,
-    truck_name VARCHAR(60) NOT NULL,
-    user_id INT, 
-    PRIMARY KEY (truck_id)
-);
+-- ****The MySQL TABLES
 
-ALTER TABLE users
-ADD FOREIGN KEY (truck_id) REFERENCES trucks(truck_id);
+-- CREATE TABLE users (
+-- 	user_id INT NOT NULL AUTO_INCREMENT,
+--     email VARCHAR(45) NOT NULL,
+--     user_name VARCHAR(30) NOT NULL, 
+--     user_pass VARCHAR(30) NOT NULL,
+--     truck_id INT, 
+--     PRIMARY KEY (user_id)
+-- );
 
-ALTER TABLE trucks
-ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
+-- CREATE TABLE trucks(
+-- 	truck_id INT NOT NULL AUTO_INCREMENT,
+--     truck_name VARCHAR(60) NOT NULL,
+--     user_id INT, 
+--     PRIMARY KEY (truck_id)
+-- );
+
+-- ALTER TABLE users
+-- ADD FOREIGN KEY (truck_id) REFERENCES trucks(truck_id);
+
+-- ALTER TABLE trucks
+-- ADD FOREIGN KEY (user_id) REFERENCES users(user_id);
 
 
-DROP TABLE IF EXISTS inventory;
+-- DROP TABLE IF EXISTS inventory;
 
-CREATE TABLE inventory(
-	inv_id INT NOT NULL AUTO_INCREMENT,
-    item_name VARCHAR(60) NOT NULL,
-    category VARCHAR(15) NOT NULL,
-    quantity INT NOT NULL,
-    unit VARCHAR(10) NOT NULL,
-    price DECIMAL(6,2) NOT NULL,
-    truck_id INT, 
-    PRIMARY KEY (inv_id),
-    FOREIGN KEY (truck_id) REFERENCES trucks(truck_id)
-);
+-- CREATE TABLE inventory(
+-- 	inv_id INT NOT NULL AUTO_INCREMENT,
+--     item_name VARCHAR(60) NOT NULL,
+--     category VARCHAR(15) NOT NULL,
+--     quantity INT NOT NULL,
+--     unit VARCHAR(10) NOT NULL,
+--     price DECIMAL(6,2) NOT NULL,
+--     truck_id INT, 
+--     PRIMARY KEY (inv_id),
+--     FOREIGN KEY (truck_id) REFERENCES trucks(truck_id)
+-- );
 
 -- ALTER TABLE inventory
 -- ADD FOREIGN KEY (truck_id) REFERENCES trucks(truck_id);
 
-CREATE TABLE expenses(
-	exp_id INT NOT NULL AUTO_INCREMENT,
-    expense_name VARCHAR(60) NOT NULL,
-    price DECIMAL(6,2) NOT NULL,
-    category VARCHAR(30) NOT NULL,
-    truck_id INT, 
-    PRIMARY KEY (exp_id),
-    FOREIGN KEY (truck_id) REFERENCES trucks(truck_id)
-);
+-- CREATE TABLE expenses(
+-- 	exp_id INT NOT NULL AUTO_INCREMENT,
+--     expense_name VARCHAR(60) NOT NULL,
+--     price DECIMAL(6,2) NOT NULL,
+--     category VARCHAR(30) NOT NULL,
+--     truck_id INT, 
+--     PRIMARY KEY (exp_id),
+--     FOREIGN KEY (truck_id) REFERENCES trucks(truck_id)
+-- );
 
 -- ALTER TABLE expenses
 -- ADD FOREIGN KEY (truck_id) REFERENCES trucks(truck_id);
 
-CREATE TABLE todo(
-	todo_id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    body VARCHAR(90) NOT NULL,
-    complete BOOLEAN NOT NULL,
-    user_id INT, 
-    truck_id INT, 
-    PRIMARY KEY (todo_id),
-    FOREIGN KEY (truck_id) REFERENCES trucks(truck_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
+-- CREATE TABLE todo(
+-- 	todo_id INT NOT NULL AUTO_INCREMENT,
+--     title VARCHAR(30) NOT NULL,
+--     body VARCHAR(90) NOT NULL,
+--     complete BOOLEAN NOT NULL,
+--     user_id INT, 
+--     truck_id INT, 
+--     PRIMARY KEY (todo_id),
+--     FOREIGN KEY (truck_id) REFERENCES trucks(truck_id),
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+-- );
 
 -- ALTER TABLE todo
 -- ADD FOREIGN KEY (truck_id) REFERENCES trucks(truck_id);
--- cruisine-app/models/User.js
+++ cruisine-app/models/User.js
@@ -1,5 +1,20 @@
 module.exports = function (sequelize, DataTypes) {
     const User = sequelize.define("User", {
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        phone: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [10, 11]
+            }
+        },
         email: {
             type: DataTypes.STRING,
             allowNull: false,
