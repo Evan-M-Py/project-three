4fd8019f603c5bcd8c92d9889eb0147516469447
Ryan <ryandub9@gmail.com>
2020-04-26 12:40:05 -0700

    adding the node server

--- cruisine-app/src/App.js
+++ cruisine-app/src/App.js
@@ -1,19 +1,14 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-<<<<<<< HEAD
-=======
-
->>>>>>> 475c0de6927ea94ffa5c48c6fbddd7e6ef4ff59b
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 // import InventoryInsert from './Components/InventoryInput';
-import  SignupForm from './Components/SignupForm'
-import LoginForm from './Components/LoginForm';
+import SignupForm from "./Components/SignupForm";
+// import LoginForm from './Components/LoginForm';
 
 function App() {
-  return (
-        // <LoginForm />
+    return (
         <SignupForm />
-  );
+    )
 }
 
 export default App;
--- cruisine-app/src/Components/LoginForm.js
+++ cruisine-app/src/Components/LoginForm.js
@@ -7,50 +7,56 @@ import Row from "react-bootstrap/Row";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Brand from "../Components/Brand";
 import Container from "react-bootstrap/Container";
-const LoginForm = props => {
+
+const LoginForm = (props) => {
     return (
-        <Container className="login d-flex align-items-center w-100">
-            <Row className="justify-content-center w-100">
-                <Jumbotron className="col-8">
-                    <Brand />
-                    <Form>
-                        <Row>
-                            <Col>
-                                <Form.Group controlId="formBasicFirstName">
-                                    <Form.Label>Username</Form.Label>
-                                    <Form.Control
-                                        type="text"
-                                        placeholder="Enter your username"
-                                    />
-                                </Form.Group>
-                            </Col>
-                            <Col>
-                                <Form.Group controlId="formBasicPassword">
-                                    <Form.Label>Password</Form.Label>
-                                    <Form.Control
-                                        type="password"
-                                        placeholder="Enter your password"
-                                    />
-                                </Form.Group>
-                            </Col>
-                        </Row>
-                        <Row className="justify-content-center">
-                            <Button
-                                className="landing-btn col-4 mt-3"
-                                variant="primary"
-                                type="submit"
-                            >
-                                Login
-                            </Button>
-                        </Row>
-                        <Row className="justify-content-center">
-                            <a class="mt-3 teal" href="#">Not a member yet? Sign up here</a>
-                        </Row>
-                    </Form>
-                </Jumbotron>
-            </Row>
-        </Container>
-    )
-}
+        <div className="login">
+            <Container className="login d-flex align-items-center w-100">
+                <Row className="justify-content-center w-100">
+                    <Jumbotron className="col-8">
+                        <Brand />
+                        <Form>
+                            <Row>
+                                <Col>
+                                    <Form.Group controlId="formBasicFirstName">
+                                        <Form.Label>Username</Form.Label>
+                                        <Form.Control
+                                            type="text"
+                                            placeholder="Enter your username"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                                <Col>
+                                    <Form.Group controlId="formBasicPassword">
+                                        <Form.Label>Password</Form.Label>
+                                        <Form.Control
+                                            type="password"
+                                            placeholder="Enter your password"
+                                        />
+                                    </Form.Group>
+                                </Col>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <Button
+                                    className="landing-btn col-4 mt-3"
+                                    variant="primary"
+                                    type="submit"
+                                >
+                                    Login
+                                </Button>
+                            </Row>
+                            <Row className="justify-content-center">
+                                <a class="mt-3 teal" href="#">
+                                    Not a member yet? Sign up here
+                                </a>
+                            </Row>
+                        </Form>
+                    </Jumbotron>
+                </Row>
+            </Container>
+        </div>
+    );
+};
 
 export default LoginForm;
+``
