794847acfde597d13cba8c142471e879d82be161
Zac Eskridge <zace118@gmail.com>
2020-04-28 19:52:57 -0700

    Almost fixed!

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 // import InventoryInsert from './Components/InventoryInput';
-import SignupForm from "./Components/SignupForm";
+// import SignupForm from "./Components/SignupForm";
 // import LoginForm from './Components/LoginForm';
+import FullAppPage from "./Components/FullAppPage/FullAppPage";
 
 
 function App() {
