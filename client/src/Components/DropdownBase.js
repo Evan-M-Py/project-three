04b3f7bf057460fc001d2bd22e12bbff1f607cab
Ryan <ryandub9@gmail.com>
2020-05-08 20:03:24 -0700

    fixed maybe?

--- client/src/Components/DropdownBase.js
+++ client/src/Components/DropdownBase.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Dropdown } from 'react-bootstrap';
-
 const DropdownBase = props => {
     return(
-        <Dropdown  className="dropdown">
-            <Dropdown.Toggle id="dropdown-button">
+        <Dropdown  style={props.style}>
+            <Dropdown.Toggle size='sm' variant="secondary" id="dropdown-basic">
                 {props.buttonLabel}
             </Dropdown.Toggle>
-
             <Dropdown.Menu >
                 {props.options.map((x) => (<Dropdown.Item onClick={(e) => props.handleDropdownChange(e.target.innerText)}>{x}</Dropdown.Item>))}
             </Dropdown.Menu>
         </Dropdown>
     )
 };
-
 export default DropdownBase;
