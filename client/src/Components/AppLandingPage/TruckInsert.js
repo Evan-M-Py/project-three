c155128dc3ab0d8f22285d91e94f11367a04b12a
Ryan <ryandub9@gmail.com>
2020-05-30 15:59:09 -0700

    deleted everything

--- client/src/Components/AppLandingPage/TruckInsert.js
+++
@@ -1,8 +0,0 @@
-import React, { useState } from "react";
-import { Button, Row, Col, Container } from 'react-bootstrap';
-
-const  TruckInsert = (props) => {
-    const [ text, setText ] = useState({
-        truck_name: ''
-    })
-};
